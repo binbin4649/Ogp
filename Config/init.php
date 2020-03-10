eb337eedba8ddf7e6310c85480f9a3d667edfc96
hideichi saito <binbin4649@gmail.com>
2020-03-10 17:05:21 +0900

    version3 support.

--- Config/init.php
+++ Config/init.php
@@ -1,2 +1,7 @@
 <?php
-$this->Plugin->initDb('Ogp');
+$BcSite = Configure::read('BcSite');
+if(substr($BcSite['version'], 0, 1) == '3'){
+	$this->Plugin->initDb('plugin', 'Ogp', array('dbDataPattern'	=> $dbDataPattern));
+}else{
+	$this->Plugin->initDb('Ogp');
+}
