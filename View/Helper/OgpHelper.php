b5048460f744d58ab2d637eddf997db4ab8f6732
hideichi saito <binbin4649@gmail.com>
2020-03-13 17:11:58 +0900

    Error correction immediately after installation.

--- View/Helper/OgpHelper.php
+++ View/Helper/OgpHelper.php
@@ -48,10 +48,19 @@ class OgpHelper extends AppHelper {
 	}
 	
 	public function ogpInfo(){
-		$OgpConfig = ClassRegistry::init('Ogp.OgpConfig');
+		$OgpConfig = ClassRegistry::init('OgpConfig');
 		$configs = $OgpConfig->find('all');
+		$return = [];
 		foreach($configs as $config){
 			$return[$config['OgpConfig']['name']] = $config['OgpConfig']['value'];
+		}
+		if(empty($return)){
+			$return = [
+				'locale' => '',
+				'locale_alternate' => '',
+				'twitter_id' => '',
+				'facebook_app_id' => '',
+			];
 		}
 		return $return;
 	}
